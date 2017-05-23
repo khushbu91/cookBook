6bec20513867473a2d44cac11d3369ec027c46a9
khushbu sharma <khushbu.sharma@L-45TWH32.jasperindia.local>
2017-05-23 11:20:40 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { SignupComponentComponent } from './signup-component/signup-component.component';
+import { SigninComponentComponent } from './signin-component/signin-component.component';
+
+
+/*const appRoutes: Routes = [
+
+      { path: 'signup', component: SignupComponentComponent },
+      { path: '',component: SigninComponentComponent },
+
+       { path: '',
+    redirectTo: '/signin',
+    pathMatch: 'full'
+  }
+  ];*/
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponentComponent,
+    SigninComponentComponent
   ],
   imports: [
+    //RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule
   ],
+  exports: [
+    RouterModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+
+export class AppModule {
+  
+ }
